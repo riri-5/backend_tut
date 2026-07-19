b992045ae7fee5e967965214e8059464177d0f57
riri-5 <157961403+riri-5@users.noreply.github.com>
2026-07-20 03:39:56 +0530

    subscriber model added

---
+++ src/models/subscription.model.js
@@ -0,0 +1,19 @@
+import mongoose, {Schema} from "mongoose";
+import { User } from "./user.model";
+
+const SubscriptionSchema = new Schema(
+    {
+        subscriber:
+        {
+            type: Schema.Types.ObjectId,
+            ref: User
+        },
+
+        channel:
+        {
+            type: Schema.Types.ObjectId,
+            ref: User
+        },
+    }, {timestamps: true});
+
+export const Subscription = mongoose.model("Subscription", SubscriptionSchema);
